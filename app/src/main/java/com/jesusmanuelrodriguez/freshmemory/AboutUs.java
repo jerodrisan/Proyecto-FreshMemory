6cb630b40358785600db0a9db96386973d7d7f66
jerodrisan <chukks@gmail.com>
2023-01-13 21:31:32 +0100

    updated files 13_1_2023

--- app/src/main/java/com/jesusmanuelrodriguez/freshmemory/AboutUs.java
+++ app/src/main/java/com/jesusmanuelrodriguez/freshmemory/AboutUs.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 
 public class AboutUs extends Activity {
 	
-	//Modified aboutUs
+	//Modified aboutUs 2
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {		
 		super.onCreate(savedInstanceState);
